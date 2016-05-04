ed9dd2e1452ba5d24257483f6013b697d858a72a
mb10 <mmbosworth+git@gmail.com>
2016-05-03 23:40:40 -0700

    testmobjs3

--- refugees360/assets/js/main.js
+++ refugees360/assets/js/main.js
@@ -1,10 +1,5 @@
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    $(document).ready(function() {
         $("body").css("background-image","none");
-
-    });
-
-
 }
 
 
