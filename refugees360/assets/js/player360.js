19fb56f6a761afad363ab89842d3c8a8492d010c
mb10 <mmbosworth+git@gmail.com>
2016-05-04 00:22:54 -0700

    removed ref360 test

--- refugees360/assets/js/player360.js
+++
@@ -1,6 +0,0 @@
-// $(window).resize(function() {
-//     var height = $(this).height();
-//     var width = $(this).width();
-//     $("iframe").css("width", width);
-//     $("iframe").css("height", height);
-// });
